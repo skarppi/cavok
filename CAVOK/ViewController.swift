e6ecb9adb406a9fd9d3e0a9d835dc01385232696
Juho Kolehmainen <juho.kolehmainen@iki.fi>
2016-09-05 14:40:26 +0300

    WhirlyGlobe 2.4.1 as submodule.

--- CAVOK/ViewController.swift
+++ CAVOK/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var mapView: WhirlyGlobeViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create an empty globe and add it to the view
+        mapView = WhirlyGlobeViewController()
+        
+        self.view.addSubview(mapView.view)
+        mapView.view.frame = self.view.bounds
+        addChildViewController(mapView)
+        
+        mapView.keepNorthUp = true
+        mapView.frameInterval = 2 // 30fps
+        mapView.threadPerLayer = true
+        mapView.autoMoveToTap = false
     }
 
     override func didReceiveMemoryWarning() {
---
+++ README.md
@@ -0,0 +1,19 @@
+# CAVOK
+
+CAVOK is an aviation weather app for iOS. It visualizes textual aviation weather observations ([METAR](https://en.wikipedia.org/wiki/METAR)) and forecasts ([TAF](https://en.wikipedia.org/wiki/Terminal_aerodrome_forecast)) on a map for quick overlook of weather in selected region.
+
+CAVOK is an abbreviation for Ceiling And Visibility OK, indicating no significant weather (thunderstorms), ceilings are greater than 5,000 ft and visibility 10 km or more.
+
+## Requirements
+
+* [Xcode 8](https://developer.apple.com/xcode)
+
+## Installation
+
+Initialize WhirlyGlobe submodules.
+
+```sh
+git submodule update --init
+cd libs/WhirlyGlobe­
+git submodule update --init
+```
